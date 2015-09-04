9fef474e2098fbf5ab228bc6c7bb53dc3adb2054
Eitr <EitrGin@gmail.com>
2015-09-04 09:18:38 -0500

    Send vertices instead of polygon sprite

--- core/src/net/eitr/gin/network/PolygonData.java
+++ core/src/net/eitr/gin/network/PolygonData.java
@@ -4,14 +4,12 @@ import com.badlogic.gdx.graphics.g2d.PolygonSprite;
 import com.badlogic.gdx.math.MathUtils;
 
 public class PolygonData extends ShapeData {
-	public PolygonSprite sprite;
+	public float [] vertices;
 	public float angle;
 	
-	public PolygonData (float px, float py, float a, PolygonSprite s) {
+	public PolygonData (float px, float py, float a, float[] v) {
 		super(px,py);
 		angle = (float)(a*180f/MathUtils.PI);
-		sprite = s;
-		sprite.setRotation(angle);
-		sprite.setPosition(x,y);
+		vertices = v;
 	}
 }
